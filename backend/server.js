9cd2f3864cb0658b0c0c9afed2091a60f2030f3e
Krish-Bagaria <Krishbagaria@gmail.com>
2026-01-16 23:48:02 +0530

    database connection and login|register|logout backend created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
 let port = process.env.PORT || 8000;
 
 let app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
 app.listen(port, () => {
   console.log("hello from server ");
+  connectDb();
 });
